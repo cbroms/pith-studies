a6dcacd4c40db48a0e16b3a1c74410cfdd72d7ba
Sydney Zheng <tlz9472@gmail.com>
2021-03-24 20:53:32 +0000

    beginning material

--- frontend/src/routes/instructions.svelte
+++ frontend/src/routes/instructions.svelte
@@ -8,21 +8,21 @@
 
 <h1>Instructions</h1>
 
+<ol>
+<li>We request you perform the session on a laptop and use either the Firefox or Chrome browser.</li>
+<li>Please read these instructions carefully. Once you are done, click the "continue" button.</li>
+<li>On the next page, you will follow a tutorial to use the platform.</li>
+<li>Once you are done with the tutorial, you will be redirected to the waiting room. The "Start" button will appear within 15 minutes. <strong>Please do not leave the waiting room during this time.</strong> Once the button appears, please click on it to join the discussion. You will be given a minute to click on the button.</li>
+<li>When you join the Pith discussion, please input a nickname. Please make sure your nickname does NOT have any personal identifiable information, such as your name or date of birth. There should be no way that your nickname can be traced back to you.</li>
+<li>You will be in the discussion room for 15 minutes with several other people. With this group, you will be completing the "Stranded" task: <strong>identify and rank the top ten items the group should take if you all were stranded on an island for a long time.</strong></li> 
+<li>At the end of the discussion, you will be redirected to a survey. It should take approximately 10 minutes. After submitting the survey, you will be redirected to Prolific to indicate the completion of your session and the submission of your response.</li>
+
+</ol>
+
 <p>
-  Soluta sed necessitatibus et. Quo fuga dolorem ut ea. Optio sit sit velit
-  voluptatibus soluta recusandae. Cumque ut suscipit amet sint nihil facilis
-  iure. Exercitationem et et vitae voluptatibus velit nostrum ut voluptatum.
-</p>
-<p>
-  Sapiente et quod sunt harum ipsam quia tempora. Ad corporis laudantium
-  excepturi. Doloremque animi quisquam non dolorum quis nisi itaque. Architecto
-  ut sequi fugit vel. Sint repellendus et quasi ipsa itaque reprehenderit
-  suscipit excepturi.
-</p>
-<p>
-  Quaerat a fugit velit quam dolor. Deserunt laboriosam voluptatem culpa non
-  officiis saepe est aut. Quos a minima totam similique. Non a qui commodi
-  laboriosam sapiente.
+<em>Payment</em>
+<br>
+Accepted responses will receive $10. After the discussion and in the survey, you will be asked to report the items your group decided to include and say why you’ve included each item. For each item that all members of the group reported was in the ranking, each member of the group will receive an additional 50 cents in bonus. Maximum ten items, or $5 bonus.
 </p>
 
 <button on:click={onContinue}>Continue</button>
