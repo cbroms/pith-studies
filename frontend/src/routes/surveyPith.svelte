84916e4dd7e9eef0df56213913477d82deda534e
Christian Broms <cb@christianbroms.com>
2021-03-24 17:52:05 -0400

    fix layout

--- frontend/src/routes/surveyPith.svelte
+++ frontend/src/routes/surveyPith.svelte
@@ -15,17 +15,17 @@
 
   const onContinue = async () => {
     const answers = {
-      "response1": response1,
-      "response2": response2,
-      "response3": response3,
-      "response4": response4,
-      "response5": response5,
-      "response5": response6,
-      "response5": response7,
-      "response5": response8,
-      "response5": response9,
-      "response5": response10,
-      "response5": response11
+      response1: response1,
+      response2: response2,
+      response3: response3,
+      response4: response4,
+      response5: response5,
+      response5: response6,
+      response5: response7,
+      response5: response8,
+      response5: response9,
+      response5: response10,
+      response5: response11,
     };
 
     await studyStore.surveyPithComplete($studyStore.pid, answers);
@@ -34,79 +34,76 @@
 
 <h1>Survey: Discussion Platform</h1>
 
-<p>
 <em>Short-answer</em>
 <ol>
   <li>
-  Please describe what you liked about the platform.
-  <input bind:value={response1} />
+    Please describe what you liked about the platform.
+    <textarea bind:value={response1} />
   </li>
   <li>
-  Please describe what you disliked about the platform.
-  <input bind:value={response2} />
+    Please describe what you disliked about the platform.
+    <textarea bind:value={response2} />
   </li>
   <li>
-  Were there any features you felt were especially useful and why?
-  <input bind:value={response3} />
+    Were there any features you felt were especially useful and why?
+    <textarea bind:value={response3} />
   </li>
   <li>
-  Were there any features in the interface that were confusing and why?
-  <input bind:value={response4} />
+    Were there any features in the interface that were confusing and why?
+    <textarea bind:value={response4} />
   </li>
   <li>
-  How does Pith compare to other similar platforms you have used, such as text messaging, Slack, Discord, Reddit, etc.?
-  <input bind:value={response5} />
+    How does Pith compare to other similar platforms you have used, such as text
+    messaging, Slack, Discord, Reddit, etc.?
+    <textarea bind:value={response5} />
   </li>
 </ol>
-</p>
 
-<p>
 <em>Multiple Choice (1 is low, 10 is high)</em>
 <ol>
   <li>
-  How easy is it to use Pith (scale of 1-10)?
-  <input type=radio bind:group={response6} value={1} />
-  <input type=radio bind:group={response6} value={2} />
-  <input type=radio bind:group={response6} value={3} />
-  <input type=radio bind:group={response6} value={4} />
-  <input type=radio bind:group={response6} value={5} />
-  <input type=radio bind:group={response6} value={6} />
-  <input type=radio bind:group={response6} value={7} />
-  <input type=radio bind:group={response6} value={8} />
-  <input type=radio bind:group={response6} value={9} />
-  <input type=radio bind:group={response6} value={10} />
+    How easy is it to use Pith (scale of 1-10)?
+    <input type="radio" bind:group={response6} value={1} />
+    <input type="radio" bind:group={response6} value={2} />
+    <input type="radio" bind:group={response6} value={3} />
+    <input type="radio" bind:group={response6} value={4} />
+    <input type="radio" bind:group={response6} value={5} />
+    <input type="radio" bind:group={response6} value={6} />
+    <input type="radio" bind:group={response6} value={7} />
+    <input type="radio" bind:group={response6} value={8} />
+    <input type="radio" bind:group={response6} value={9} />
+    <input type="radio" bind:group={response6} value={10} />
   </li>
 </ol>
-</p>
 
 <p>
-Please note down any other observations you would like to share.
-<input bind:value={response7} />
+  Please note down any other observations you would like to share.
+  <textarea bind:value={response7} />
 </p>
 
 <h1>Survey: Pilot</h1>
 
-<p>
 <em>Short-answer</em>
 <ol>
   <li>
-  Did you use an external aid, such as a notepad, to complete the answers to this survey? Note that answering "yes" will not result in a loss of bonus.
-  <input bind:value={response8} />
+    Did you use an external aid, such as a notepad, to complete the answers to
+    this survey? Note that answering "yes" will not result in a loss of bonus.
+    <textarea bind:value={response8} />
   </li>
   <li>
-  Were there any technical difficulties during the course of your session? If so, please describe what they were.
-  <input bind:value={response9} />
+    Were there any technical difficulties during the course of your session? If
+    so, please describe what they were.
+    <textarea bind:value={response9} />
   </li>
   <li>
-  Was there anything else about the study you felt could be improved?
-  <input bind:value={response10} />
+    Was there anything else about the study you felt could be improved?
+    <textarea bind:value={response10} />
   </li>
 </ol>
-</p>
 
 <p>
-Please note down any other observations you would like to share.
-<input bind:value={response11} />
+  Please note down any other observations you would like to share.
+  <textarea bind:value={response11} />
 </p>
 
 <button on:click={onContinue}>Done</button>
--- frontend/src/routes/surveyTask.svelte
+++ frontend/src/routes/surveyTask.svelte
@@ -10,12 +10,12 @@
 
   const onContinue = async () => {
     const answers = {
-      "response0": response0,
-      "response1": response1,
-      "response2": response2,
-      "response3": response3,
-      "response4": response4,
-      "response5": response5
+      response0: response0,
+      response1: response1,
+      response2: response2,
+      response3: response3,
+      response4: response4,
+      response5: response5,
     };
 
     await studyStore.surveyTaskComplete($studyStore.pid, answers);
@@ -24,58 +24,54 @@
 
 <h1>Survey: Task</h1>
 
-<p>
 <em>Short-answer</em>
 <ol>
   <li>
-  <p>
-  <em>(Required)</em> Please input the nickname you used during the discussion.
-  </p>
-  <input bind:value={response0} />
+    <p>
+      <em>(Required)</em> Please input the nickname you used during the discussion.
+    </p>
+    <input bind:value={response0} />
   </li>
   <li>
-  <p>
-  Please report the items your group decided to include in the final ranking. Justify each item in about a sentence.
-  </p>
-  <input bind:value={response1} />
+    <p>
+      Please report the items your group decided to include in the final
+      ranking. Justify each item in about a sentence.
+    </p>
+    <textarea bind:value={response1} />
   </li>
 </ol>
-</p>
 
-<p>
 <em>Multiple Choice ((1 is low, 5 is high)</em>
 <ol>
   <li>
-  Please rate how good you think the group plan is (1-5).
-  <input type=radio bind:group={response2} value={1} />
-  <input type=radio bind:group={response2} value={2} />
-  <input type=radio bind:group={response2} value={3} />
-  <input type=radio bind:group={response2} value={4} />
-  <input type=radio bind:group={response2} value={5} />
+    Please rate how good you think the group plan is (1-5).
+    <input type="radio" bind:group={response2} value={1} />
+    <input type="radio" bind:group={response2} value={2} />
+    <input type="radio" bind:group={response2} value={3} />
+    <input type="radio" bind:group={response2} value={4} />
+    <input type="radio" bind:group={response2} value={5} />
   </li>
   <li>
-  Please rate how much you feel you have contributed to the group discussion (1-5).
-  <input type=radio bind:group={response3} value={1} />
-  <input type=radio bind:group={response3} value={2} />
-  <input type=radio bind:group={response3} value={3} />
-  <input type=radio bind:group={response3} value={4} />
-  <input type=radio bind:group={response3} value={5} />
+    Please rate how much you feel you have contributed to the group discussion
+    (1-5).
+    <input type="radio" bind:group={response3} value={1} />
+    <input type="radio" bind:group={response3} value={2} />
+    <input type="radio" bind:group={response3} value={3} />
+    <input type="radio" bind:group={response3} value={4} />
+    <input type="radio" bind:group={response3} value={5} />
   </li>
   <li>
-  Please rate how much you felt your ideas were heard by the group (1-5).
-  <input type=radio bind:group={response4} value={1} />
-  <input type=radio bind:group={response4} value={2} />
-  <input type=radio bind:group={response4} value={3} />
-  <input type=radio bind:group={response4} value={4} />
-  <input type=radio bind:group={response4} value={5} />
+    Please rate how much you felt your ideas were heard by the group (1-5).
+    <input type="radio" bind:group={response4} value={1} />
+    <input type="radio" bind:group={response4} value={2} />
+    <input type="radio" bind:group={response4} value={3} />
+    <input type="radio" bind:group={response4} value={4} />
+    <input type="radio" bind:group={response4} value={5} />
   </li>
 </ol>
-</p>
 
-<p>
-Please note down any other observations you would like to share.
-<input bind:value={response5} />
-</p>
+<p>Please note down any other observations you would like to share.</p>
+<textarea bind:value={response5} />
 
 <!-- Give error if response0, for nickname, is null. -->
-<button on:click={onContinue}>Continue</button>
+<div><button on:click={onContinue}>Continue</button></div>
