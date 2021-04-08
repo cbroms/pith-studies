bc0c73bbbbfed37ad8751eee6a19a107cea322fe
Christian Broms <cb@christianbroms.com>
2021-04-08 14:05:49 -0400

    fix iframe copy issue

--- frontend/src/routes/study/[id]/discussion.svelte
+++ frontend/src/routes/study/[id]/discussion.svelte
@@ -2,4 +2,10 @@
   import { studyStore } from "../../../stores/studyStore";
 </script>
 
-<iframe title="discussion" src={$studyStore.discussionURL} />
+<iframe
+  title="discussion"
+  allow="clipboard-read; clipboard-write self https://current.pith.is"
+  src={$studyStore.discussionURL}
+/>
+
+<!--   -->
